fe32c80ac1100c4bb4a4163aa864c12e1c3d84d3
Rickard Dybeck <r.dybeck@gmail.com>
2010-10-21 21:37:03 +0200

    forgot

---
+++ src/main/java/com/unknown/entity/raids/RaidRewardListener.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.unknown.entity.raids;
+
+/**
+ *
+ * @author alde
+ */
+public interface RaidRewardListener {
+
+        public void onRaidInfoChanged();
+}
